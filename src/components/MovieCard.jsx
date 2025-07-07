1326a6ddc9e78c8baf9b7b529543d485d2224b44
Abhiraj Patel <abhi0808raj@gmail.com>
2025-07-07 15:31:55 +0530

    Fixed Favorite Bug

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ import {useMovieContext} from "../contexts/MovieContext.jsx";
 function MovieCard({movie}) {
     const{isFavorite,addToFavorites,removeFromFavorites} = useMovieContext();
     const favorite = isFavorite(movie.id)
-    function onFavouriteClick(e){
+    function onFavoriteClick(e){
         e.preventDefault()
         if(favorite) removeFromFavorites(movie.id)
         else addToFavorites(movie)
@@ -15,7 +15,7 @@ function MovieCard({movie}) {
                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
                 <div className="movie-overlay">
                     <button className={`favourite-btn${favorite?"active":""}`} onClick={onFavouriteClick}>
-                        🤍
+                        ♥
                     </button>
                 </div>
             </div>
